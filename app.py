b0f7fc072873a278af7ed584f342fc7828ae1370
danielwcorrea <danielwcorrea@gmail.com>
2025-05-23 09:57:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,6 @@ def extenso():
         return jsonify({"extenso": valor})
     except Exception as e:
         return jsonify({"erro": str(e)}), 400
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
