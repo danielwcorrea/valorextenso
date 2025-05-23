d31caf0d6fccdb9f732ee9963fb9410044f68030
danielwcorrea <danielwcorrea@gmail.com>
2025-05-23 09:16:09 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from num2words import num2words
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API para converter número por extenso"
+
+@app.route("/extenso")
+def extenso():
+    numero = request.args.get('numero')
+    idioma = request.args.get('lang', 'pt')
+    try:
+        valor = num2words(int(numero), lang=idioma)
+        return jsonify({"extenso": valor})
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 400
